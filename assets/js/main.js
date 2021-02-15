35ebfcad1f7dfeb8daff47de1c5c5d4aa5e61b98
Marc-André Charpentier <emma.charp@me.com>
2021-02-15 18:07:54 -0500

    Passer au javascript vanille

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,165 +1,192 @@
-$(document).keyup(function(e) {
-	if(e.keyCode === 27) $('body').toggleClass('debug');
-});
+const body = document.body;
 
+document.addEventListener('keyup', function(e){
+	if(e.code === 'Escape') body.classList.toggle('debug');
+});
 
 (function ($) {
 	'use strict';
 	var ready = function () {
 		// ajouter livereload seulement si je suis en local
-		if (window.location.href.indexOf(".local") > -1 || window.location.href.indexOf("localhost") > -1) {
-			$('body').append('<script src="http://localhost:35729/livereload.js"></script>');
+		if(window.location.href.indexOf('.local') > -1 || window.location.href.indexOf('localhost')) {
+			const script = document.createElement('script');
+			script.setAttribute('src', 'http://localhost:35729/livereload.js');
+			body.append(script);
 		}
 
-		function ChangeValueFactor(t) {
-			var value = (t.val() - t.attr('min')) / (t.attr('max') - t.attr('min'));
-			t.css('--value', value);
+		function changeValueFactor(t) {
+			const value = (t.value - t.getAttribute('min')) / (t.getAttribute('max') - t.getAttribute('min'));
+			t.style.setProperty('--value', value);
 		}
 
 		function setDefault(item, set_model = true) {
 			var t = item;
-			var name = t.find('input, select').attr('name');
-			var auto = t.find('input[checked]');
-			var preset = t.find('input').first().attr('value') || t.find('select').find('option').first().text();
-			var step = (t.find('input[type="range"]')) ? t.find('input[type="range"]').attr('step') : 1;
-			var value = (auto.length != 0) ? auto.attr('value') : preset;
-			var parent_section = t.parents('article');
-			var interactive_model = parent_section.find('.interactive-model');
-			var target = (t.parents('.targetable-controls').length) ? interactive_model.children() : interactive_model;
+			var field = t.querySelector('input, select');
+			var name = field.getAttribute('name');
+			var auto = t.querySelector('input[checked]');
+			var preset = (field.tagName == 'INPUT') ? field.getAttribute('value') : field.querySelector('option').textContent;
+			var step = (t.querySelector('input[type="range"]')) ? t.querySelector('input[type="range"]').getAttribute('step') : 1;
+			var value = (auto) ? auto.getAttribute('value') : preset;
+			var parent_section = t.closest('article');
+			var interactive_model = parent_section.querySelectorAll('.interactive-model');
+			var target = (t.closest('.targetable-controls')) ? interactive_model[0].children : interactive_model;
 
-			var property = t.find('input, select').attr('name');
+			var property = t.querySelector('input, select').getAttribute('name');
 
-			if(t.find('input').is('[min="-360"][max="360"]')) {
+			if(field.matches('[min="-360"][max="360"]')) {
 				value += 'deg';
 			}
-			else if(t.find('input').is('[min="0"][max="5000"]')) {
+			else if(field.matches('[min="0"][max="5000"]')) {
 				value += 'ms';
 			}
-			else if(!isNaN(value) && Number.isInteger(parseFloat(step)) && parseInt(t.find('input').attr('max')) > 10) {
+			else if(!isNaN(value) && Number.isInteger(parseFloat(step)) && parseInt(t.querySelector('input').getAttribute('max')) > 10) {
 				value += 'px';
 			}
 
-			if(t.find('output').length == 0) {
-				t.find('div').prepend('<output for="'+name+'"></output>');
+			if(!t.querySelector('output')) {
+				var output = document.createElement('output');
+				output.setAttribute('for', name);
+				t.querySelector('div').prepend(output);
 			}
-			t.find('input').each(function(){
-				$(this).val($(this).attr('value'));
+			[...t.querySelectorAll('input')].forEach((field) => {
+				field.value = field.getAttribute('value');
 			});
-			t.find('output').html(value);
+			t.querySelector('output').innerHTML = value;
 
 			if (set_model == true) {
-				target.css('--'+property, value);
+				[...target].forEach((element) => {
+					element.style.setProperty('--'+property, value);
+				});
 			}
 		}
 
-		$('.model-controls input[type="range"]').each(function() {
-			ChangeValueFactor($(this));
+		[...document.querySelectorAll('.model-controls input[type="range"]')].forEach((input) => {
+			changeValueFactor(input);
 		});
 
-		$('.model-controls li:has(input, select)').each(function() {
-			setDefault($(this));
+		var controlItems = document.querySelectorAll('.model-controls li');
+
+		controlItems.forEach((el) => {
+			if (el.querySelector('select, input')) {
+				setDefault(el);
+			}
 		});
 
-		$('input, select').on('input', function(){
-			var t = $(this);
-			var value = (t.attr('type') == 'range') ? this.valueAsNumber : this.value;
-			var output = t.parents('li').find('output');
-			var property = t.attr('name');
-			var step = (t.attr('type') == 'range') ? t.attr('step') : 1;
-			var parent_section = t.parents('article');
-			var interactive_model = parent_section.find('.interactive-model');
-			var target = (t.parents('.targetable-controls').length) ? interactive_model.children() : interactive_model;
-			if(t.is('[type="range"],[type="text"]')) {
-				t.parents('li').find('input[type="checkbox"]').prop('checked', false);
-			}
+		document.querySelectorAll('input, select').forEach((item) => {
+			item.addEventListener('input', function(e){
+				var t = e.target;
+				var value = (t.getAttribute('type') == 'range') ? this.valueAsNumber : this.value;
+				var output = t.closest('li').querySelector('output');
+				var property = t.getAttribute('name');
+				var step = (t.getAttribute('type') == 'range') ? t.getAttribute('step') : 1;
+				var parent_section = t.closest('article');
+				var interactive_model = parent_section.querySelectorAll('.interactive-model');
+				var target = (t.closest('.targetable-controls')) ? interactive_model[0].children : interactive_model;
+				if(t.matches('[type="range"],[type="text"]') && t.closest('li').querySelector('input[type="checkbox"]')) {
+					t.closest('li').querySelector('input[type="checkbox"]').checked = false;
+				}
 
-			if(t.is('[type="checkbox"]:not(:checked)')) {
-				value = t.parents('li').find('input[type="range"]').val();
-			}
+				if(t.matches('[type="checkbox"]:not(:checked)')) {
+					value = t.closest('li').querySelector('input[type="range"]').value;
+				}
 
-			if(t.is('[min="-360"][max="360"]')) {
-				value += 'deg';
-			}
-			else if(t.is('[min="0"][max="5000"]')) {
-				value += 'ms';
-			}
-			else if(!isNaN(value) && Number.isInteger(parseFloat(step)) && parseFloat(step) >= 10) {
-				value += 'px';
-			}
+				if(t.matches('[min="-360"][max="360"]')) {
+					value += 'deg';
+				}
+				else if(t.matches('[min="0"][max="5000"]')) {
+					value += 'ms';
+				}
+				else if(!isNaN(value) && Number.isInteger(parseFloat(step)) && parseFloat(step) >= 10) {
+					value += 'px';
+				}
 
-			output.html(value);
+				output.innerHTML = value;
 
-			if (t.parents('details').is('.targeted')) {
-				target.filter('.selected').css('--'+property, value);
-			} else {
-				target.css('--'+property, value);
-			}
-
+					if (t.closest('.targeted')) {
+						[...target].forEach((element) => {
+							if(element.matches('.selected')) {
+								element.style.setProperty('--'+property, value);
+							}
+						});
+					} else {
+						[...target].forEach((element) => {
+							element.style.setProperty('--'+property, value);
+						});
+					}
+			});
 		});
 
-		$('input[type="range"]').on('mouseup keyup', function () {
-			ChangeValueFactor($(this));
-		});
-
-		$('input, summary', '.model-controls').on('focus', function(){
-			$(this).parentsUntil('.model-controls').addClass('is-focused');
-			$(this).parents('.model-controls').addClass('is-in-use');
-		});
-
-		$('input, summary', '.model-controls').on('blur', function(){
-			$(this).parentsUntil('.model-controls').removeClass('is-focused');
-			$(this).parents('.model-controls').removeClass('is-in-use');
-		});
-
-		$('.selectable-items').on('click', 'p', function(){
-			var t = $(this);
-			var parent_section = t.parents('article');
-			var model_controls = parent_section.find('.model-controls');
-			var targeted_controls = model_controls.find('.targetable-controls')
-			var style_string = t.attr('style') + t.parents('.interactive-model').attr('style');
-			// var styles = style_string.slice(0, style_string.length - 1).split(';').map(x => x.split(':'))
-
-			$('li:has(input)', targeted_controls).each(function() {
-				setDefault($(this), false);
+		document.querySelectorAll('input[type="range"]').forEach((item) => {
+			item.addEventListener('mouseup', function(e){
+				changeValueFactor(item);
 			});
 
-			if (style_string !== undefined && style_string != '') {
-				var parts = style_string.split(";");
-
-				for (var i=0;i<parts.length - 1;i++) {
-					var subParts = parts[i].split(':');
-					var name = subParts[0].replace('--', '').replace(' ', '');
-					var value = subParts[1];
-					var target_input = targeted_controls.find('input[name="' + name + '"]');
-					var refInputType = target_input.attr('type');
-
-					if (refInputType == 'radio' || refInputType == 'checkbox') {
-						targeted_controls.find('input[name="' + name + '"][value = "' + value + '"]').prop('checked', true);
-					}
-
-					else if (refInputType == 'range' || refInputType == 'text') {
-						targeted_controls.find('input[name="' + name + '"]').first().val(value);
-					}
-
-					targeted_controls.find('output[for="' + name + '"]').text(value);
-				}
-			}
-
-			if(!event.shiftKey == 1) {
-				t.siblings().removeClass('selected');
-			}
-
-			if(t.is('.selected') && t.siblings('.selected').length == 0) {
-				targeted_controls.removeClass('targeted');
-			} else {
-				targeted_controls.addClass('targeted');
-			}
-
-			t.toggleClass('selected');
-
+			item.addEventListener('keyup', function(e){
+				changeValueFactor(item);
+			});
 		});
+
+		var selectableItems = document.querySelector('.selectable-items');
+		if(selectableItems) {
+			document.querySelector('.selectable-items').addEventListener('click', function(e) {
+				if(e.target.closest('p')) {
+					var t = e.target;
+					var parent_section = t.closest('article');
+					var model_controls = parent_section.querySelector('.model-controls');
+					var targeted_controls = model_controls.querySelector('.targetable-controls');
+					var style_string = t.getAttribute('style') + t.closest('.interactive-model').getAttribute('style');
+					// var styles = style_string.slice(0, style_string.length - 1).split(';').map(x => x.split(':'))
+
+					var controlItems = targeted_controls.querySelectorAll('li');
+					// $('li:has(input)', targeted_controls).each(function() {
+					controlItems.forEach((el) => {
+						if(el.querySelector('input')) {
+							setDefault(el, false);
+						}
+					});
+
+					if (style_string !== undefined && style_string != '') {
+						var parts = style_string.split(";");
+
+						for (var i=0;i<parts.length - 1;i++) {
+							var subParts = parts[i].split(':');
+							var name = subParts[0].replace('--', '').replace(' ', '');
+							var value = subParts[1];
+							var target_input = targeted_controls.querySelector('input[name="' + name + '"]');
+							var refInputType = (target_input) ? target_input.getAttribute('type') : null;
+							var target_output = targeted_controls.querySelector('input[name="' + name + '"]');
+							if (refInputType == 'radio' || refInputType == 'checkbox') {
+								targeted_controls.querySelector('input[name="' + name + '"][value = "' + value + '"]').checked = true;
+							}
+
+							else if (refInputType == 'range' || refInputType == 'text') {
+								targeted_controls.querySelector('input[name="' + name + '"]').value = value;
+							}
+
+							else if (target_output) {
+								target_output.innerHTML = value;
+							}
+						}
+					}
+
+					if(!event.shiftKey == 1 && t.closest('.interactive-model').querySelector('.selected')) {
+						[...t.closest('.interactive-model').querySelectorAll('.selected')].forEach((element) => {
+							element.classList.remove('selected');
+						});
+					}
+
+					if(t.matches('.selected')) {
+						targeted_controls.classList.remove('targeted');
+					} else {
+						targeted_controls.classList.add('targeted');
+					}
+
+					t.classList.toggle('selected');
+				}
+			});
+		}
 	}
 
-	$(ready);
+	ready();
 
-})(jQuery);
+})();
